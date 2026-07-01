4af010ea075f87a118f187a1921a7aa668c68a98
axloojoaquin-dotcom <axloojoaquin@gmail.com>
2026-07-01 13:11:11 -0300

    Update PublicacionService.java

--- src/main/java/desi/DevNada/tp/servicios/PublicacionService.java
+++ src/main/java/desi/DevNada/tp/servicios/PublicacionService.java
@@ -17,28 +17,28 @@ public class PublicacionService {
 
     @Autowired
     private IPropiedadRepo propiedadRepo;
-    // Epic 2.4 - ListadoPublicaciones
+   
     public List<Publicacion> obtenerTodas() {
         return publicacionRepo.findAll();
     }
 
-    // Epic 2.1 - AltaPublicación
+    
     
 public void guardarPublicacion(Publicacion publicacion) throws Exception {
         
-        // 1. 
+        
         Propiedad propiedadReal = propiedadRepo.findById(publicacion.getPropiedad().getId())
                 .orElseThrow(() -> new Exception("La propiedad seleccionada no existe en la base de datos."));       // 3. 
         publicacion.setPropiedad(propiedadReal);
         publicacionRepo.save(publicacion);
     }
- // Epic 2.2 - 
+  
     public Publicacion obtenerPorId(Long id) throws Exception {
         return publicacionRepo.findById(id)
                 .orElseThrow(() -> new Exception("La publicación solicitada no existe."));
     }
 
-    // Epic 2.3 - Eliminar 
+    
     public void eliminar(Long id) {
         publicacionRepo.deleteById(id);
     }
